e65e75a3ac4889219ddb869888c102effcc543d6
Cirec-Coder <cirec.coder@gmail.com>
2024-06-21 12:54:03 +0200

    refactoring

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -17,42 +17,26 @@ const HoverEffect = () => {
     </div>
   );
 };
+
+const GalleryItem = (imgName: string) => {
+  return (
+    <div className="relative group overflow-hidden">
+      <img
+        src={imgName}
+        alt="mage 1"
+        className="group-hover:scale-110 duration-500"
+      />
+      <HoverEffect />
+    </div>
+  );
+};
 const Gallery = () => {
   return (
     <section>
       <div className="grid grid-cols-1 place-items-center sm:grid-cols-2 md:grid-cols-4">
-        <div className="relative group overflow-hidden">
-          <img
-            src={GalleryImg1}
-            alt="mage 1"
-            className="group-hover:scale-110 duration-500"
-          />
-          <HoverEffect />
-        </div>
-        <div className="relative group overflow-hidden">
-          <img
-            src={GalleryImg2}
-            alt="image 2"
-            className="group-hover:scale-110 duration-500"
-          />
-          <HoverEffect />
-        </div>
-        <div className="relative group overflow-hidden">
-          <img
-            src={GalleryImg3}
-            alt=""
-            className="group-hover:scale-110 duration-500"
-          />
-          <HoverEffect />
-        </div>
-        <div className="relative group overflow-hidden">
-          <img
-            src={GalleryImg4}
-            alt=""
-            className="group-hover:scale-110 duration-500"
-          />
-          <HoverEffect />
-        </div>
+        {[GalleryImg1, GalleryImg2, GalleryImg3, GalleryImg4].map((img) =>
+          GalleryItem(img)
+        )}
       </div>
     </section>
   );
