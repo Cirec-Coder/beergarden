bcec70dd012643577fdaabc05caa81700b6f386b
Cirec-Coder <cirec.coder@gmail.com>
2024-06-26 20:33:32 +0200

    close menu when click outside

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,34 +1,71 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { cn } from "../lib/utils";
 import { navItems } from "../constants";
 import { Icons } from "./Icons";
 
 const MobileNav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      !(
+        (event.target as HTMLElement).id.startsWith("menu") ||
+        (event.target as HTMLElement).parentElement?.id.startsWith("menu")
+      ) &&
+      ref.current &&
+      !ref.current.contains(event.target as Node)
+    ) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setMenuOpen(false);
+    };
+
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
       <div
+        id="menu-btn"
         className="sm:hidden cursor-pointer text-white"
         onClick={() => setMenuOpen((prev) => !prev)}
       >
-        {menuOpen ? <Icons.XMark /> : <Icons.Bars />}
+        {menuOpen ? (
+          <Icons.XMark id="menu-btn1" />
+        ) : (
+          <Icons.Bars id="menu-btn2" />
+        )}
       </div>
       <div
+        ref={ref}
         className={cn(
           "absolute top-full w-40 bg-black bg-opacity-50 duration-700 transition-all",
           { "translate-x-0 left-0": menuOpen },
           { "-translate-x-full left-0": !menuOpen }
         )}
+        onClick={(event) => event.stopPropagation()}
       >
         <ul className="px-10 py-5 space-y-2">
           {navItems.map((item) => (
-            <li key={item.id}>
+            <li
+              key={item.id}
+              onClick={() => setMenuOpen(false)}
+            >
               <a
                 className="text-white hover:text-zinc-400 duration-300"
                 href={item.link}
                 target="_self"
-                onClick={() => setMenuOpen(false)}
               >
                 {item.name}
               </a>
@@ -44,21 +81,30 @@ const MobileNav = () => {
             target="_blank"
             className="text-white hover:text-zinc-400 duration-300"
           >
-            <Icons.Twitter width={20} height={20} />
+            <Icons.Twitter
+              width={20}
+              height={20}
+            />
           </a>
           <a
             href="https://www.facebook.com/"
             target="_blank"
             className="text-white hover:text-zinc-400 duration-300"
           >
-            <Icons.FacebookF width={20} height={20} />
+            <Icons.FacebookF
+              width={20}
+              height={20}
+            />
           </a>
           <a
             href="https://www.instagram.com/"
             target="_blank"
             className="text-white hover:text-zinc-400 duration-300"
           >
-            <Icons.Instagram width={20} height={20} />
+            <Icons.Instagram
+              width={20}
+              height={20}
+            />
           </a>
         </div>
       </div>
